74d9baabaa2b056eac0af367b2fd3cdfda3b4c9b
PM-Devs <princegh100@outlook.com>
2025-06-03 22:21:17 +0000

    all......ddd

--- main.py
+++ main.py
@@ -123,4 +123,4 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("index:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
